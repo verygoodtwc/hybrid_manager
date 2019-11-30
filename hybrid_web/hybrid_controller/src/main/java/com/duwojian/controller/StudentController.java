85ed99c10a02f0a6a63918cfc110b02d1db19e03
twc <123456123@qq.com>
2019-11-30 15:48:59 +0800

    完成增加方法

--- hybrid_web/hybrid_controller/src/main/java/com/duwojian/controller/StudentController.java
+++ hybrid_web/hybrid_controller/src/main/java/com/duwojian/controller/StudentController.java
@@ -1,11 +1,15 @@
 package com.duwojian.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.duwojian.ClassEntity;
 import com.duwojian.StudentEntity;
+import com.duwojian.service.IClassService;
 import com.duwojian.service.IStudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("student")
@@ -13,9 +17,13 @@ public class StudentController {
 
     @Reference
     private IStudentService studentService;
+    @Reference
+    private IClassService classService;
 
     @RequestMapping("toadd")
     private String  toadd(Model model){
+        List<ClassEntity> list = classService.list();
+        model.addAttribute("list",list);
         return "studentadd";
     }
     @RequestMapping("add")
@@ -24,6 +32,10 @@ public class StudentController {
         System.out.println(studentService);
         boolean save = studentService.save(studentEntity);
         if(save){
+            ClassEntity byId = classService.getById(studentEntity.getCid());
+            int people=byId.getPeople()+1;
+            byId.setPeople(people);
+            classService.updateById(byId);
             return "ok";
         }
         return null;
